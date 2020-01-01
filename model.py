1795ab7af9bb856d62a47b0797a7c868b668eb2b
zhmhbest <zhmhbest@gmail.com>
2020-01-01 19:38:10 +0800

    update

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://<帐号>:<密码>@<HOST>:3306/<数据库名称>'
+db = SQLAlchemy(app)
 
 
 @app.route("/")
