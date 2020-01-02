e699315032de7a7e6110887016019dffa078491b
zhmhbest <zhmhbest@gmail.com>
2020-01-02 17:58:23 +0800

    模板过滤器

--- demo_template.py
+++ demo_template.py
@@ -9,15 +9,41 @@ app = Flask(
 
 @app.route("/")
 def index():
+    return """
+    <ul>
+        <li><a href='./hello'>Hello Template</a></li>
+        <li><a href='./filter'>Filter Template</a></li>
+    </ul>
+    """
+
+
+@app.route("/hello")
+def interface_template_hello():
     data = {
         'title': "Hello Template",
         'test_list': ['a', 'b', 'c'],
         'test_dict': {
             'key1': 'val1',
             'key2': 'val2',
-        }
+        },
+        'a': 1,
+        'b': 2,
     }
-    return render_template("template1.html", **data)
+    return render_template("hello.html", **data)
+
+
+@app.route("/filter")
+def interface_template_filter():
+    data = {
+        'PI': 3.1415,
+        'escape': "<span style='color: red'>Escape String<span>",
+        'hello': "hello",
+        'HELLO': "HELLO",
+        'i_love_you': "i love you",
+        'space': "    space    ",
+        'test_list': [3, 2, 1, 8, 4, 5],
+    }
+    return render_template("filter.html", **data)
 
 
 if __name__ == '__main__':
